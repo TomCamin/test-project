8390c58c771d194e19f5839f8df077cbf82ad967
Tom <tom.camin@gmail.com>
2012-10-03 15:24:33 +0200

    add api types and add vegetal

--- src/Arcamy/HomeBundle/Controller/RestController.php
+++ src/Arcamy/HomeBundle/Controller/RestController.php
@@ -3,6 +3,7 @@
 namespace Arcamy\HomeBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use FOS\RestBundle\View\View;
 
 class RestController extends Controller
@@ -50,4 +51,36 @@ class RestController extends Controller
         
         return $view;
     } // "get_vegetal"    [GET] /vegetal/{id}
+    
+    public function postVegetalAction(Request $request)
+    {       
+        $em = $this->getDoctrine()->getEntityManager();
+        $type = $em->getRepository('HomeBundle:Type')->find($request->request->get('type'));
+        
+        $vegetable = new \Arcamy\HomeBundle\Entity\Vegetal();
+        $vegetable->setName($request->request->get('name'));
+        $vegetable->setDescription($request->request->get('description'));
+        $vegetable->setType($type);
+        $vegetable->setCreatedAt(Date());
+        
+        $em->persist($vegetable);
+        $em->flush();
+        
+        $view = View::create()  
+          ->setStatusCode(200);  
+        
+        return $view;
+    } // "new_vegetal"    [GET] /vegetal/new
+    
+    public function getTypesAction()
+    {
+        $em = $this->getDoctrine()->getEntityManager();
+        $type = $em->getRepository('HomeBundle:Type')->findAll();
+        
+        $view = View::create()  
+          ->setStatusCode(200)  
+          ->setData($type);  
+        
+        return $view;
+    } // "get_types"    [GET] /types
 }
